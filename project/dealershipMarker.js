c345b03cf2d3abc4ddd31b2f66a8e525420745f8
AdrianK77 <145582138+AdrianK77@users.noreply.github.com>
2024-10-26 15:31:11 -0400

    Added all components for portfolio website

---
+++ project/dealershipMarker.js
@@ -0,0 +1,103 @@
+// StAuth10244: I Adrian Kobylt, 000833903 certify that this material is my original work. No other person's work has been used without due acknowledgement. I have not made my work available to anyone else.
+const dealershipMarker = [
+    {
+        "DEALERSHIP_ID": 1,
+        "NAME": "Budds' BMW Hamilton",
+        "ADDRESS": "1471 Upper James St, Hamilton, ON L9B 1K2",
+        "CATEGORY": "European_Dealership",
+        "WEBSITE": "https://www.buddsbmwhamilton.com/?utm_source=google_lso_trader&utm_medium=organic&utm_campaign=home",
+        "COMMENTS": "This is a car dealership that sells German vehicles by the automotive brand BMW.",
+        "LONGITUDE": -79.8910009,
+        "LATITUDE": 43.2067824
+    },
+    {
+        "DEALERSHIP_ID": 2,
+        "NAME": "Audi Hamilton",
+        "ADDRESS": "1215 Upper James St, Hamilton, ON L9C 3B2",
+        "CATEGORY": "European_Dealership",
+        "WEBSITE": "https://www.audihamilton.com/",
+        "COMMENTS": "This is a car dealership that sells German vehicles by the automotive brand AUDI.",
+        "LONGITUDE": -79.8873093,
+        "LATITUDE": 43.2150703
+    },
+    {
+        "DEALERSHIP_ID": 3,
+        "NAME": "Leggat Mazda Burlington",
+        "ADDRESS": "805 Walkers Line, Burlington, ON L7N 2G1",
+        "CATEGORY": "Japanese_Dealership",
+        "WEBSITE": "https://www.upperjamestoyota.ca/?utm_source=google&utm_medium=organic&utm_campaign=google_my_business",
+        "COMMENTS": "This is a car dealership that sells Japanese vehicles by the automotive brand Mazda.",
+        "LONGITUDE": -79.7828645,
+        "LATITUDE": 43.3629051
+    },
+    {
+        "DEALERSHIP_ID": 4,
+        "NAME": "Subaru of Hamilton",
+        "ADDRESS": "2482 Upper James Street, Hamilton, ON L9B 1K9",
+        "CATEGORY": "Japanese_Dealership",
+        "WEBSITE": "https://www.subaruhamilton.com/",
+        "COMMENTS": "This is a car dealership that sells Japanese vehicles by the automotive brand Subaru.",
+        "LONGITUDE": -79.9048289,
+        "LATITUDE": 43.1774698
+    },
+    {
+        "DEALERSHIP_ID": 5,
+        "NAME": "Image Honda",
+        "ADDRESS": "155 Centennial Pkwy N, Hamilton, ON L8E 1H8",
+        "CATEGORY": "Japanese_Dealership",
+        "WEBSITE": "https://shorturl.at/woIDm",
+        "COMMENTS": "This is a car dealership that sells Japanese vehicles by the automotive brand Honda.",
+        "LONGITUDE": -79.7618037,
+        "LATITUDE": 43.2342512
+    },
+    {
+        "DEALERSHIP_ID": 6,
+        "NAME": "Mohawk Ford",
+        "ADDRESS": "930 Upper James St, Hamilton, ON L9C 3A5",
+        "CATEGORY": "American_Dealership",
+        "WEBSITE": "https://www.mohawkford.com/",
+        "COMMENTS": "This is a car dealership that sells American vehicles by the automotive brand Ford.",
+        "LONGITUDE": -79.8834211,
+        "LATITUDE": 43.225087
+    },
+    {
+        "DEALERSHIP_ID": 7,
+        "NAME": "Eastgate Ford",
+        "ADDRESS": "350 Parkdale Ave N, Hamilton, ON L8H 5Y3",
+        "CATEGORY": "American_Dealership",
+        "WEBSITE": "https://eastgateford.com/",
+        "COMMENTS": "This is a car dealership that sells American vehicles by the automotive brand Ford.",
+        "LONGITUDE": -79.7875426,
+        "LATITUDE": 43.2465719
+    },
+    {
+        "DEALERSHIP_ID": 8,
+        "NAME": "Dewildt Chrysler Dodge Jeep Ram",
+        "ADDRESS": "1600 Main St E, Hamilton, ON L8K 1E7",
+        "CATEGORY": "American_Dealership",
+        "WEBSITE": "https://shorturl.at/CMMMg",
+        "COMMENTS": "This is a car dealership that sells a variety of American vehicles from brands such as Dodge, Jeep, and Chrysler.",
+        "LONGITUDE": -79.80179,
+        "LATITUDE": 43.237274
+    },
+    {
+        "DEALERSHIP_ID": 9,
+        "NAME": "Fairview Chrysler Dodge Limited",
+        "ADDRESS": "2377 Fairview St, Burlington, ON L7R 2E3",
+        "CATEGORY": "American_Dealership",
+        "WEBSITE": "https://www.fairviewchrysler.com/",
+        "COMMENTS": "This is a car dealership that sells a variety of American vehicles from brands such as Dodge, Jeep, and Chrysler.",
+        "LONGITUDE": -79.8008901,
+        "LATITUDE": 43.3453703
+    },
+    {
+        "DEALERSHIP_ID": 10,
+        "NAME": "Queenston Chevrolet Buick GMC",
+        "ADDRESS": "2260 Rymal Rd E, Hannon, ON L0R 1P0",
+        "CATEGORY": "American_Dealership",
+        "WEBSITE": "https://www.queenstonchev.com/",
+        "COMMENTS": "This is a car dealership that sells a variety of American vehicles from brands such as Chevrolet, Buick, and GMC.",
+        "LONGITUDE": -79.794976,
+        "LATITUDE": 43.186163
+    }
+]
